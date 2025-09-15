c9d640203ae56beb80e5115db010952ad0e09f65
Poojitha Nakkina <Nakkina.Poojitha@esyasoft.com>
2025-09-15 21:47:59 +0530

    OOPS for banking acc

---
+++ .Net/oops_assign/Classs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops_assign
+{
+    internal class Classs
+    {
+        private int accountNumber;
+        private string name;
+        private int balance;
+
+        public Classs(int accNum, string name, int initialDeposit)
+        {
+            this.accountNumber = accNum;
+            this.name = name;
+            this.balance = initialDeposit;
+        }
+
+        public void Deposit(int amount)
+        {
+            balance += amount;
+            Console.WriteLine($"Deposited amount: {amount}");
+            Console.WriteLine($"Balance: {balance}");
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrawn amount: {amount}");
+                Console.WriteLine($"Balance: {balance}");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("\n----- Account Details -----");
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Account Number: {accountNumber}");
+            Console.WriteLine($"Balance: {balance}");
+            Console.WriteLine("---------------------------\n");
+        }
+
+        public int GetAccountNumber() => accountNumber;
+    }
+}
---
+++ .Net/oops_assign/Program.cs
@@ -0,0 +1,125 @@
+﻿namespace oops_assign
+{
+    internal class Program
+    {
+        static Classs account;
+        static int accNum = 4740;
+        static void Main(string[] args)
+        {
+            bool flag = true;
+
+            while (flag)
+            {
+                Console.WriteLine("Hello, Welcome to Bank!");
+                Console.WriteLine("1) Create an account");
+                Console.WriteLine("2) Deposit");
+                Console.WriteLine("3) Withdraw");
+                Console.WriteLine("4) Display Account Details");
+                Console.WriteLine("5) Exit");
+                Console.Write("Choose A Number: ");
+
+                string num = Console.ReadLine();
+
+                switch (num)
+                {
+                    case "1":
+                        CreateAccount();
+                        break;
+                    case "2":
+                        Deposit();
+                        break;
+                    case "3":
+                        Withdraw();
+                        break;
+                    case "4":
+                        DisplayDetails();
+                        break;
+                    case "5":
+                        flag = false;
+                        Console.WriteLine("Thank you for using our Bank!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+            }
+        }
+
+        static void CreateAccount()
+        {
+            Console.WriteLine("Enter your Name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter amount to be deposited:");
+            int initialDeposit = Convert.ToInt32(Console.ReadLine());
+
+            account = new Classs(accNum, name, initialDeposit);
+
+            Console.WriteLine("Account Successfully Created!");
+        }
+
+        static void Deposit()
+        {
+            if (account == null)
+            {
+                Console.WriteLine("No account exists. Please create an account first.");
+                return;
+            }
+
+            Console.WriteLine("Enter your Account number:");
+            int acc = Convert.ToInt32(Console.ReadLine());
+
+            if (acc == account.GetAccountNumber())
+            {
+                Console.WriteLine("Enter amount to deposit:");
+                int amount = Convert.ToInt32(Console.ReadLine());
+                account.Deposit(amount);
+            }
+            else
+            {
+                Console.WriteLine("Wrong Account Number");
+            }
+        }
+
+        static void Withdraw()
+        {
+            if (account == null)
+            {
+                Console.WriteLine("No account exists. Please create an account first.");
+                return;
+            }
+
+            Console.WriteLine("Enter your Account number:");
+            int acc = Convert.ToInt32(Console.ReadLine());
+
+            if (acc == account.GetAccountNumber())
+            {
+                Console.WriteLine("Enter amount to withdraw:");
+                int amount = Convert.ToInt32(Console.ReadLine());
+                account.Withdraw(amount);
+            }
+            else
+            {
+                Console.WriteLine("Wrong Account Number");
+            }
+        }
+
+        static void DisplayDetails()
+        {
+            if (account == null)
+            {
+                Console.WriteLine("No account exists. Please create an account first.");
+                return;
+            }
+
+            account.DisplayDetails();
+        }
+
+    }
+}
+
+
+
+
+
+
