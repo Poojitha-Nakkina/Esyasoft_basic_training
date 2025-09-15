e04f08854283e8d14ba230a7d1d0ee57a095de4c
Poojitha Nakkina <Nakkina.Poojitha@esyasoft.com>
2025-09-15 13:23:24 +0530

    Classes constructors

---
+++ .Net/class_demo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace class_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            //student first = new student();
+            //first.initialize();
+            //first.showDisplay();
+
+            student first = new student(12, "Pooj",22,"12th","9876543");
+                first.showDisplay();
+
+        }
+    }
+}
---
+++ .Net/class_demo/student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_demo
+{
+    internal class student
+    {
+       int studentid;
+         string name;
+        int age;
+        string standard;
+        string contactnum;
+        
+        public void initialize()
+        {
+            studentid = 1;
+            name = "abcd";
+
+        }
+
+        public void showDisplay()
+        {
+            Console.WriteLine("Displaying Objects");
+            Console.WriteLine(name);
+        }
+
+
+        public student(int std_id , string std_name,int std_age, string std,string cn)
+        {
+            studentid= std_id;
+            name = std_name;
+            age = std_age;
+            standard = std;
+            contactnum = cn;
+        }
+
+        //public int getMarks()
+        //{
+
+        //}
+
+    }
+}
