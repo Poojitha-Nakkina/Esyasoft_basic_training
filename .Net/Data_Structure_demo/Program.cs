ac939abe00bee4538a8b0f98be149922e1525bf0
Poojitha Nakkina <Nakkina.Poojitha@esyasoft.com>
2025-09-18 12:58:19 +0530

    Data structure assignment

---
+++ .Net/Data_Structure_demo/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Data_Structure_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List Example
+            List<Student> students = new List<Student>();
+
+            // Add Student objects to the list
+            Student first = new Student(1, "Alice", 10);
+            Student second = new Student(2, "Bob", 90);
+            Student third = new Student(3, "Charlie", 78);
+            students.Add(first);
+            students.Add(second);
+            students.Add(third);
+
+
+            // Access and display each student using foreach
+            Console.WriteLine("Student List:");
+            foreach (Student s in students)
+            {
+                Console.WriteLine($"ID: {s.id}, Name: {s.name}, Marks: {s.marks}");
+            }
+
+            // Access a specific object by index
+            Console.WriteLine($"\nSecond student is: {students[1].name}");
+
+
+            //Dictionay Example
+            Dictionary<string, Student> students_dict = new Dictionary<string, Student>();
+            students_dict.Add("firstStudent", first);
+            students_dict.Add("seondStudent", second);
+            students_dict.Add("thirdStudent", third);
+
+
+            foreach (KeyValuePair<string, Student> student in students_dict)
+            {
+                Console.WriteLine(student.Value.id);
+            }
+
+
+            //Hashset Example
+            Console.WriteLine("Hashset demo");
+            HashSet<Student> students_hashset = new HashSet<Student>();
+            students_hashset.Add(first);
+            students_hashset.Add(second);
+            students_hashset.Add(first);
+            students_hashset.Add(third);
+
+            foreach (Student student in students_hashset)
+            {
+                Console.WriteLine(student.id);
+            }
+
+
+            //StackDemo
+
+
+            Stack<Student> students_stack = new Stack<Student>();
+            students_stack.Push(first);
+            students_stack.Push(second);
+            students_stack.Push(third);
+
+            Student pop_stack = students_stack.Pop();
+            Console.WriteLine(pop_stack.name);
+
+
+            //Queue demo
+            Console.WriteLine("Queue demo");
+            Queue<Student> students_queue = new Queue<Student>();
+            students_queue.Enqueue(first);
+            students_queue.Enqueue(second);
+            students_queue.Enqueue(third);
+
+            Console.WriteLine(students_queue.Dequeue().name);
+
+            //LinkedList demo
+            Console.WriteLine("Linked List");
+            LinkedList<Student> students_ll = new LinkedList<Student>();
+            students_ll.AddFirst(first);
+            students_ll.AddFirst(second);
+            students_ll.AddFirst(third);
+
+            foreach (Student student in students_ll)
+            {
+                Console.WriteLine(student.id);
+            }
+            Console.WriteLine(students_ll.First().name);
+
+            //Tuple
+            Console.WriteLine("Tuple");
+            Tuple<Student,Student,Student> students_tp = new Tuple<Student,Student,Student>(first,second,third);
+            Console.WriteLine(students_tp.Item1.name);
+
+
+
+        }
+    }
+}
---
+++ .Net/Data_Structure_demo/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structure_demo
+{
+    internal class Student
+    {
+        public int id;
+       public  string name;
+        public int marks;
+
+
+        public Student(int std_id, string std_name, int score)
+        {
+            id = std_id;
+            name = std_name;
+            marks = score;
+        }
+    }
+}
