a9868073173ca385bfe2832e3565b9d09165ec19
Poojitha Nakkina <Nakkina.Poojitha@esyasoft.com>
2025-09-18 09:51:01 +0530

    dlms first assignment

---
+++ Embedded_C/dlmss/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace dlmss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // OBIS code for Active Energy Import (1-0:1.8.0.255)
+            var obis = new ObisCode(1, 0, 1, 8, 0, 255);
+
+            // Class ID for Register object is 3
+            ushort classId = 3;
+            byte attributeId = 2; // e.g., value attribute
+
+            byte[] apdu = GetRequestApdu.CreateGetRequest(obis, classId, attributeId);
+
+            Console.WriteLine("OBIS Code: " + obis);
+            Console.WriteLine("GET Request APDU: " + BitConverter.ToString(apdu));
+        }
+    }
+}
