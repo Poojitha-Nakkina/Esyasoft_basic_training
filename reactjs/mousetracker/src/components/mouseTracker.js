59bdbfaca186522740160c83e8a635c22a84bbe2
Poojitha Nakkina <Nakkina.Poojitha@esyasoft.com>
2025-09-22 12:11:57 +0530

    MouseTracker assign

---
+++ reactjs/mousetracker/src/components/mouseTracker.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+const MouseTracker = () => {
+ 
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+
+  useEffect(() => {
+   
+    const handleMouseMove = (event) => {
+      setMousePosition({ x: event.clientX, y: event.clientY });
+    };
+
+   
+    window.addEventListener('mousemove', handleMouseMove);
+
+    
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h2>Mouse Tracker</h2>
+      <p>Move your mouse around!</p>
+      <p>
+        Current Mouse Position: X: {mousePosition.x}, Y: {mousePosition.y}
+      </p>
+    </div>
+  );
+};
+
+export default MouseTracker;
